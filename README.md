eaeb36423d1b5c950ab393a89baf2a02ba17d3b1
cq_bomb <collinsctk@gmail.com>
2019-06-22 19:57:01 +0800

    test for Python

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# python1
-* 用于各种实验测试
+# **自我练习区域**
+###### 在Windows和Linux下创建一个Python_First.py的代码，计算 1 + 2的结果，并且打印到屏幕上。
+* ######[百度] (https://qytsystem.qytang.com/static/files/images/pythonhomework/1_Python%E5%9F%BA%E7%A1%80%20%E7%AC%AC%E4%B8%80%E5%A4%A9/1.png)
--- pythontest.py
+++ pythontest.py
@@ -1,3 +1,8 @@
-print('Welcome to qytang!')
-print('test for git!')
-print('from git')
+import random
+section1 = random.randint(0,255)
+section2 = random.randint(0,255)
+section3 = random.randint(0,255)
+section4 = random.randint(0,255)
+
+random_ip = str(section1) + '.' + str(section2) + '.' + str(section3) + '.' + str(section4)
+print(random_ip)
